e5ba742a466e5e03dbce0022e7e212d8c7a92573
leviongit <levi.duncan.self@gmail.com>
2023-04-01 05:05:31 +0200

    rubify the code a bit

--- ryuyi.rb
+++ ryuyi.rb
@@ -2,18 +2,12 @@
 
 require 'discordrb'
 
-bottoken = File.open('ryuyi-api-token.txt').read
-bottoken.strip!
+bottoken = File.read('ryuyi-api-token.txt').strip
 
 bot = Discordrb::Bot.new token: bottoken
 
-bot.mention() do |event|
-  highest_role = 0
-  event.message.author.roles.each { |role|
-    if role.position > highest_role
-      highest_role = role.position
-    end
-  }
+bot.mention do |event|
+  highest_role = event.message.author.roles.map(&:position).max
 
   if highest_role < 31
     event.message.create_reaction("\u{1F44E}")  # thumbs down
