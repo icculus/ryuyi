370e8151d0aea4a008e24211d35c885eef27c7cd
Ryan C. Gordon <icculus@icculus.org>
2023-03-31 22:56:04 -0400

    remove debug logging

--- ryuyi.rb
+++ ryuyi.rb
@@ -8,31 +8,6 @@ bottoken.strip!
 bot = Discordrb::Bot.new token: bottoken
 
 bot.mention() do |event|
-  puts("Saw a mention!")
-  txt = ''
-  txt << 'text='
-  txt << event.message.content
-  txt << ',username='
-  if event.message.author.username.nil?
-    txt << '(nil)'
-  else
-    txt << event.message.author.username
-  end
-  txt << ',nick='
-  if event.message.author.nick.nil?
-    txt << '(nil)'
-  else
-    txt << event.message.author.nick
-  end
-  txt << ',roles='
-  event.message.author.roles.each { |role|
-    txt << role.name
-    txt << '('
-    txt << role.position.to_s
-    txt << '),'
-  }
-  puts(txt)
-
   highest_role = 0
   event.message.author.roles.each { |role|
     if role.position > highest_role
